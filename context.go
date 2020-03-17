b1e3ae0f540a372a0b201459df9319efe1ecc356
ipfans <363344+ipfans@users.noreply.github.com>
2020-03-17 09:02:38 +0800

    feat: support Copy context

--- context.go
+++ context.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 )
 
+type contextKey struct{}
+
 type Context struct {
 	context.Context
 
@@ -12,11 +14,22 @@ type Context struct {
 	m    map[string]interface{}
 }
 
+var ctxKey = contextKey{}
+
 func New(ctx context.Context) *Context {
 	return &Context{
 		Context: ctx,
 		m:       map[string]interface{}{},
 	}
+}
+
+func Copy(ctx context.Context) *Context {
+	c := New(ctx)
+	val := ctx.Value(ctxKey)
+	if val != nil {
+		c.m = val.(map[string]interface{})
+	}
+	return c
 }
 
 func (c *Context) Set(key string, val interface{}) {
@@ -35,6 +48,15 @@ func (c *Context) Exists(key string) bool {
 func (c *Context) Value(i interface{}) interface{} {
 	key, ok := i.(string)
 	if !ok {
+		if _, ok := i.(contextKey); ok {
+			c.lock.RLock()
+			defer c.lock.RUnlock()
+			newMap := make(map[string]interface{}, len(c.m))
+			for k := range c.m {
+				newMap[k] = c.m[k]
+			}
+			return newMap
+		}
 		return c.Context.Value(i)
 	}
 	c.lock.RLock()
